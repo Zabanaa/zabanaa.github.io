f3e47957053d7353e8a88fbe08fa986793deef05
MilkshakePanda <iam.superpeluche@gmail.com>
2016-03-12 13:14:34 +0100

    Add animations on desktop

--- _dev/js/yo.js
+++ _dev/js/yo.js
@@ -2,7 +2,7 @@
 
 let showElements = () => {
 
-    let scrollTop = window.scrollY // save the scroll top value
+    let scrollTop = window.pageYOffset // save the scroll top value
     let windowHeight = window.innerHeight // save the windows height
     let sections = Array.from(document.querySelectorAll('.animated-section')) // save the sections top offset value
 
@@ -18,15 +18,9 @@ let showElements = () => {
 
             })
 
-            console.log(section.childNodes)
-
-
         }
-        // if the scroll top is higher than the section top offset, that means the element is in the view port
     })
 
-    // What we want to calculate is the when the window scroll top is higher than the offset top of the section - (20 of the windows height)
-        // then we add the animate class to the children
 }
 
 window.addEventListener('scroll', showElements);
